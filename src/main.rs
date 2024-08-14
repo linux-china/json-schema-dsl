4a0801da22db295e2004e442a05f0955a57cb2fc
Libing Chen <libing.chen@gmail.com>
2024-08-14 23:55:22 +0800

    chore: add `-v`, `--version` support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::env::args;
 
+const VERSION: &str = "0.2.1";
+
 fn main() {
     let args: Vec<String> = args().collect();
     if args.len() < 2 {
         println!("json-schema-dsl \"User{{id: int}}\"");
+        return;
+    }
+    if args[1] == "-v" || args[1] == "--version" {
+        println!("json-schema-dsl {}", VERSION);
         return;
     }
     let schema = args[1].as_str();
